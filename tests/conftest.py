b20be5837233024335593d580f2db827afe1018e
Michal <michal.gryszowka@gmail.com>
2023-03-09 19:38:24 +0100

    Fixture open_and_login_std_user added

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.login_page import LoginPage
+from technical.shop_user_model import UserName
+from technical.shop_user_model import Password
 
 
 @fixture
@@ -15,3 +17,11 @@ def init_driver():
 @fixture
 def get_login_page(init_driver):
     return LoginPage(init_driver)
+
+
+@fixture
+def open_and_login_std_user(get_login_page):
+    std_user_inv_page = get_login_page.log_in_user(UserName.user_1, Password.pwd)
+    return std_user_inv_page
+
+
